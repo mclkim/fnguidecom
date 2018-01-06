85bcefe1e266973f9453917bb355142ac1330135
zwolf21 <pbr112@naver.com>
2018-01-07 01:24:39 +0900

    Thread pool excutor

--- api/__main__.py
+++ api/__main__.py
@@ -1,10 +1,30 @@
+import argparse
+
 try:
 	from rank import get_rank_table, save
 except:
 	from .rank import get_rank_table, save
 
 def main():
-	get_rank_table()
+    argparser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="fnGuide 유틸"
+    )
+    argparser.add_argument('-O', '--output', help='엑셀파일로 저장',  nargs='?')
+    argparser.add_argument('-W', '--workers', type=int, help='최대스레드수',  nargs='?', default=10)
+    args = argparser.parse_args()
+
+    output_path = args.output
+    max_worker = args.workers
+    print('Starting with {} Workers...'.format(max_worker))
+
+    df_rank = get_rank_table(max_worker=max_worker)
+
+    if df_rank.empty:
+        print('Cannot retrive ranking info')
+    else:
+        save(df_rank, output_path)
+
 
 if __name__ == '__main__':
 	main()
--- api/rank.py
+++ api/rank.py
@@ -1,11 +1,15 @@
-import re, datetime
+import os, re, datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-import requests
+import requests, tqdm
 from bs4 import BeautifulSoup
 from urllib.parse import *
 
 import pandas as pd
 import numpy as np
+
+
+MAX_WORKERS = 10
 
 
 def _get_upjong_codemap():
@@ -27,7 +31,7 @@ def _get_upjong_codemap():
     return ret
 
 
-def _get_rank_analytics(upjong_cd, cmdText='menu_6_1'):
+def _get_rank_analytics(upjong_cd, upjong_nm, cmdText='menu_6_1'):
     url = 'http://comp.fnguide.com/SVO2/common/sp_read_json.asp'
     params = {
         'cmdText': cmdText,
@@ -39,44 +43,57 @@ def _get_rank_analytics(upjong_cd, cmdText='menu_6_1'):
     }
     r = requests.get(url, params=params)
     soup = BeautifulSoup(r.content, 'html.parser')
-    return r.json()
-
-
-def annotate_upjong_info(cmdText='menu_6_1'):
-    codemap = _get_upjong_codemap()
-    dfs = []
-    for cdmap in codemap:
-        for cd, nm in cdmap.items():
-            rank = _get_rank_analytics(cd, cmdText=cmdText)
-            rank_df = pd.DataFrame(rank)
-            rank_df['업종코드'] = cd
-            rank_df['업종명'] = nm
-            dfs.append(rank_df)
-    df = pd.concat(dfs, ignore_index=True)
+    df = pd.DataFrame(r.json())
+    df['업종명'] = upjong_nm
+    df['업종코드'] = upjong_cd
     return df
 
 
-def get_rank_table():
-    print('get_rank_table')
-#     rank_by_upjong = annotate_upjong_info('menu_6_1')
-#     rank_by_index = annotate_upjong_info('menu_6_2')
-#     rank_by_upjong.set_index(['업종코드', 'GICODE'], inplace=True)
-#     rank_by_index.set_index(['업종코드', 'GICODE'], inplace=True)
-#     rest_columns = sorted(set(rank_by_index.columns) - set(rank_by_upjong.columns))
-#     rank_by_index = rank_by_index[rest_columns]
-#     return pd.merge(rank_by_upjong, rank_by_index, left_index=True, right_index=True)
+def annotate_upjong_info(cmdText='menu_6_1', max_worker=None):
+    codemap = _get_upjong_codemap()
+    dfs = []
+    max_worker = max_worker or MAX_WORKERS
+    workers = min(max_worker, len(codemap))
+    print('workers:', workers)
+    with ThreadPoolExecutor(workers) as executor:
+        todo_list = []
+        for cdmap in codemap:
+            for cd, nm in cdmap.items():
+                future = executor.submit(_get_rank_analytics, cd, nm, cmdText)
+                todo_list.append(future)
+        done_iter = tqdm.tqdm(as_completed(todo_list), total=len(todo_list))
+        for future in done_iter:
+            df = future.result()
+            dfs.append(df)
+        return pd.concat(dfs, ignore_index=True)
 
 
-def save(df, prefix='Ranking Analytics'):
+def get_rank_table(ntry=3, max_worker=None):
+    print('trying to retrive ranging..(ntry: {})'.format(ntry))
+    if ntry == 0:
+        return
+    try:
+        print('retriving upjoing table...')
+        rank_by_upjong = annotate_upjong_info('menu_6_1', max_worker=max_worker)
+        print('retriving index table...')
+        rank_by_index = annotate_upjong_info('menu_6_2', max_worker=max_worker)
+    except:
+        print('faild to retriving retrying...')
+        return get_rank_table(ntry-1, max_worker=max_worker)
+    else:
+        print('retrive success!')
+        rank_by_upjong.set_index(['업종코드', 'GICODE'], inplace=True)
+        rank_by_index.set_index(['업종코드', 'GICODE'], inplace=True)
+        rest_columns = sorted(set(rank_by_index.columns) - set(rank_by_upjong.columns))
+        rank_by_index = rank_by_index[rest_columns]
+        return pd.merge(rank_by_upjong, rank_by_index, left_index=True, right_index=True)
+
+
+def save(df, to='.', prefix='Ranking Analytics'):
     now = datetime.datetime.now()
     str_now = now.strftime('%Y-%m-%d %H=%M=%S')
     fname = '{} {}.xlsx'.format(prefix, str_now)
-    df.to_excel(fname)
-
-
-
-
-
-
-
+    path = os.path.join(to, fname)
+    print('saving to {}...'.format(path))
+    df.to_excel(path)
 
