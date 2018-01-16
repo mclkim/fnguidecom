3dfd9e6c07041ba8f42220f6b41a87567d2707e4
zwolf21 <pbr112@naver.com>
2018-01-16 14:11:27 +0900

    ok

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.2',
 
     # description='Tracking your .git changed history',
     # long_description=get_long_description(),
@@ -86,7 +86,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'bs4', 'pandas', 'tqdm'],
+    install_requires=['requests', 'bs4', 'pandas', 'tqdm', 'openpyxl'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
